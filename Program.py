712ecfb3f2acc333562cc42cf8bd1567478632c1
Rodolfo880 <rodolforodriguezculqui@gmail.com>
2026-04-27 02:10:05 -0500

    first commit

---
+++ Program.py
@@ -0,0 +1,7 @@
+edad : int = 20
+altura : float = 1.80
+nombre : str = "Carlos"
+apellido = 'Chavez'
+esVerde : bool = True
+
+print (f"{nombre} {apellido} tiene {edad} años y su carro {"es" if esVerde else "No es"}verde")
